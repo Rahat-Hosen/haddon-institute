9c90ce247e597f9f99080e688110d6a90a60e170
zpuckeridge <zacchary@puckeridge.me>
2023-07-17 06:07:50 +0000

    feat: clerk + prisma basic implementation

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+
+export default authMiddleware({
+  afterAuth(auth, req, evt) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  },
+
+  publicRoutes: ["/", "/unauthorised", "/sign-in", "/sign-up"],
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
